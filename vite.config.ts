ec2ac216d1870f18e46f2e38e910d587501c9d29
James Payne <github@jepaynedev.com>
2026-05-11 15:59:59 -0500

    Add GitHub Pages deployment action

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 
 export default defineConfig({
+  base: '/adsb-skycam/',
   plugins: [react()],
   resolve: {
     alias: {
